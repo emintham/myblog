7b872c51541db49fb67dc049855b451d04753d7a
Emin Tham <emintham@emintham>
2025-05-10 15:01:24 -0700

    seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://your-blog-url.com', // TODO: Replace with your actual site URL
+  integrations: [
+    sitemap()
+  ],
+
+});
