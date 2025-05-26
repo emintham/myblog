8d4a5e7a2b14aa0da54ed2a31aa316ea4e045d6e
Emin Tham <emintham@emintham>
2025-05-25 17:48:08 -0700

    fix mdx not showing up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://your-blog-url.com", // TODO: Replace with your actual site URL
-  integrations: [sitemap(), react()],
+  integrations: [sitemap(), mdx(), react()],
 });
