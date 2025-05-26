a78a418f090aa23a904d90b9f75c0a2d7c7c0a54
Emin Tham <emintham@emintham>
2025-05-26 10:36:49 -0700

    test cloudflare build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emintham.com',
+
   integrations: [
     sitemap(),
     mdx(),
     react()
   ],
+
+  adapter: cloudflare(),
 });
